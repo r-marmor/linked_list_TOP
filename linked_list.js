9dc50cf038a5c21f32525dbce88d345d7e9ad1c4
r-marmor <raphael.marmor@gmail.com>
2023-03-02 17:57:18 +0100

    finished implementing all but extra credit

--- linked_list.js
+++ linked_list.js
@@ -8,7 +8,7 @@ class LinkedList {
         const newNode = new Node(value);
         this.length++;
 
-        // Case there's no node yet, create one at the head
+        // Case there's no node yet
         if (this.head == null) {
            this.head = newNode;
         // if there's one, append it at the end of the list
@@ -56,10 +56,8 @@ class LinkedList {
 
     at(index) {
         // checks if the index is correct
-        if (index < 0 || index > this.length - 1) {
-            console.error(`index must be between 0 and ${this.length - 1}`);
-            return;
-        }
+        if (index < 0 || index > this.length - 1) return null;
+        
         // error msg if there is no node in the list
         if (this.head == null) return null;
             else {
@@ -105,6 +103,20 @@ class LinkedList {
         return null;
     }
 
+    toString() {
+            // Case there's no node
+            if (this.head == null) return null;
+
+            let pointer = this.head;
+            let result = "";
+
+            for (let i = 0; i < this.length; i++) {
+                result += "(" + pointer.value + ") -> ";
+                pointer = pointer.nextNode;
+            }
+            return result += "null";
+    }
+
 }
 
 class Node {
@@ -117,10 +129,16 @@ class Node {
 let myLinkedList = new LinkedList();
 
 
-myLinkedList.append(1);
-myLinkedList.append(2);
-myLinkedList.append(3);
-myLinkedList.append(4);
-myLinkedList.append(5);
 
-console.log(myLinkedList.find(5));
+// myLinkedList.append(2);
+// myLinkedList.prepend("HEAD");
+// myLinkedList.append(3);
+// myLinkedList.append(4);
+// myLinkedList.append("POP ME");
+// console.log(myLinkedList.toString());
+// myLinkedList.pop();
+// myLinkedList.append("TAIL");
+// console.log(myLinkedList.toString());
+
+// console.log(myLinkedList.listHead());
+// console.log(myLinkedList.find(4));
